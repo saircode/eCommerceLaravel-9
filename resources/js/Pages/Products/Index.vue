da3c800d8072e4274a336ff3ddf0f601af072a5d
saircode <sairjsanchez@gmail.com>
2023-05-02 15:01:32 -0500

    Controlador, ruta y vista de productos

---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,21 @@
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+export default {
+    components: {
+        AppLayout
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<template>
+    <AppLayout title="Productos">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Listado de productos
+            </h2>
+        </template>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -32,4 +33,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+});
+
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',
+])->group(function () {
+    Route::get('/productos', [ProductController::class , 'index'])->name('products.index');
 });
